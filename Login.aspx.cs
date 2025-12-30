a4e7081cf177785227e7434adff343d0b94b6ef0
AlonsoNajera <isc.najera@outlook.com>
2025-12-30 14:11:49 -0600

    f

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -34,7 +34,7 @@ namespace SoftwarePlantas
                     return;
                 }
 
-                lblVersion.Text = "LICENCIA ACTIVA - Version 3.1";
+                lblVersion.Text = "LICENCIA ACTIVA - Version 3.3";
             }
         }
 
